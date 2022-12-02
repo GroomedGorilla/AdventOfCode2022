ceff4071b2561a0aed3fe71ec4e79c4f0fb5b98c
Julian Zammit <julian.zammit@fundingcircle.com>
2022-12-02 15:58:12 +0000

    Day 1 - Most Cals per elf

---
+++ 1/soln-1.py
@@ -0,0 +1,18 @@
+from pathlib import Path
+
+p = Path(__file__).with_name("input.txt")
+elves = []
+sum = 0
+
+with p.open('r') as f:
+    while line := f.readline():
+        if line.strip():
+            sum += int(line)
+        else:
+            elves.append(sum)
+            sum = 0
+    elves.append(sum)
+
+chonkmaster = elves.index(max(elves))
+elves.sort
+print(f'Chonkiest elf is num {chonkmaster + 1} with {max(elves)} Calories ')
